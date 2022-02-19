f7281137bdeac1116a426c6ba2d6a59a1c279b98
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-19 14:59:43 -0600

    Adds Page Thumbnails to Drawer UI

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Harold Hopson on 2/19/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
